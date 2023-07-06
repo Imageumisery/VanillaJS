b15ec9912b49af01290a162fd944e6ee31e728c4
imageumisery <imageumisery@gmail.com>
2023-07-06 16:25:22 +0500

    grocery bud done

--- grocery bud/app.js
+++ grocery bud/app.js
@@ -1,43 +1,44 @@
 const groceryList = document.querySelector('.grocery-list');
+const input = document.querySelector('#grocery');
 const submit = document.querySelector('.submit-btn');
-const form = document.querySelector('.grocery-form');
 const clearBtn = document.querySelector('.clear-btn');
 const alert = document.querySelector('.alert');
 const defaultItemName = 'grocery-item';
 const dataIdName = 'data-id';
+const titleName = 'title';
 const defaulClass = 'class';
 const editIcon = "fas fa-edit";
 const editBtn = "edit-btn";
 const deleteIcon = "fas fa-trash";
 const deleteBtn = "delete-btn";
+const show = 'show';
 
 let listOfGroceries = [];
 let idList = [];
-let dataId = 0;
+let dataId = - 1;
+let editableItemId;
+let editing = false;
 
 //clicks
-submit.addEventListener('click', createItem);
+submit.addEventListener('click', handleSubmit);
+input.addEventListener('keydown', onEnter)
 clearBtn.addEventListener('click', clearAll);
 
+//appear buttons if any item exists
+setInterval(checkItemList, 50);
 
-function createItem() {
-    const item = document.createElement('article');
-    const title = document.createElement('p');
-    title.classList.add('title');
+
+function handleSubmit() {
     let inputValue = document.querySelector('#grocery').value;
-    if (inputValue === '') {
-        return
+    if (inputValue === '') return;
+    if (editing) {
+        editItem(inputValue);
+        editing = false;
+    } else {
+        createItem(inputValue);
     }
-    //should terminate value with only spaces
-    title.textContent = inputValue;
-
-    item.appendChild(title);
-    item.classList.add(defaultItemName);
-    item.setAttribute(dataIdName, ++dataId);
-    idList.push(dataId);
-    item.appendChild(addButtonContainer());
-    groceryList.appendChild(item);
-    document.querySelector('#grocery').value = '';
+    //clearing input field
+    input.value = '';
 }
 
 function addButtonContainer() {
@@ -51,9 +52,8 @@ function addButtonContainer() {
     icon2.setAttribute(defaulClass, deleteIcon);
     btn1.classList.add(editBtn);
     btn2.classList.add(deleteBtn);
-    btn1.addEventListener('click', edit);
-    btn2.addEventListener('click', clearItem);
-
+    btn1.addEventListener('click', onEdit);
+    btn2.addEventListener('click', deleteItem);
 
     btn1.appendChild(icon1);
     btn2.appendChild(icon2);
@@ -63,16 +63,63 @@ function addButtonContainer() {
     return btnContainer;
 }
 
-function clearItem() {
+function onEnter(e) {
+    if (e.key === "Enter") {
+        handleSubmit();
+    }
+}
 
+function deleteItem() {
+    let id = this.parentElement.parentElement.dataset.id;
+    let item = document.querySelector(`[data-id='${id}']`);
+    console.log(item);
+    item.remove();
+    delete listOfGroceries[id];
 }
 
 function clearAll() {
     while (groceryList.firstChild) {
         groceryList.firstChild.remove();
     }
+    listOfGroceries = [];
+    dataId = -1;
+    idList = [];
 }
 
-function edit() {
+function onEdit() {
+    editing = true;
+    editableItemId = this.parentElement.parentElement.dataset.id;
+    input.value = listOfGroceries[editableItemId];
+}
 
+function editItem(inputValue) {
+    let item = document.querySelector(`[data-id='${editableItemId}']`);
+    item.firstChild.textContent = inputValue;
+}
+
+function createItem(inputValue) {
+    const item = document.createElement('article');
+    const title = document.createElement('p');
+    title.classList.add(titleName);
+    title.textContent = inputValue;
+    //should terminate value with only spaces
+
+    item.appendChild(title);
+    item.classList.add(defaultItemName);
+    item.setAttribute(dataIdName, ++dataId);
+    idList.push(dataId);
+    listOfGroceries.push(inputValue);
+    console.log(listOfGroceries);
+
+    item.appendChild(addButtonContainer());
+    groceryList.appendChild(item);
+}
+
+function checkItemList() {
+    let items = groceryList.firstChild;
+    if (items) {
+        clearBtn.classList.add(show);
+    } else {
+        clearBtn.classList.remove(show);
+    }
 }
